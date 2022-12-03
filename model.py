25f636e83b534a950ae3b679de5865140067e585
aya oshima <ayaoshima0221@gmail.com>
2022-12-03 12:58:51 -0500

    up-to-date codes

--- model.py
+++ model.py
@@ -6,10 +6,44 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 
-class ConvInputModel(nn.Module): # Module: Pytorch Base class for all neural network modules.
+class BiggerConvInputModel(nn.Module):
+    def __init__(self):
+        super(BiggerConvInputModel, self).__init__()
+        
+        #CNN Layers for sort of clevr task from the original paper
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.batchNorm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=2, padding=1)
+        self.batchNorm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
+        self.batchNorm3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=2, padding=1)
+        self.batchNorm4 = nn.BatchNorm2d(256)
+
+        
+    def forward(self, img):
+        """convolution"""
+        x = self.conv1(img)
+        x = F.relu(x)
+        x = self.batchNorm1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.batchNorm2(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.batchNorm3(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.batchNorm4(x)
+        return x
+
+
+
+class ConvInputModel(nn.Module):
     def __init__(self):
         super(ConvInputModel, self).__init__()
         
+        #CNN Layers for sort of clevr task for a smaller network
         self.conv1 = nn.Conv2d(3, 24, 3, stride=2, padding=1)
         self.batchNorm1 = nn.BatchNorm2d(24)
         self.conv2 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
@@ -36,7 +70,8 @@ class ConvInputModel(nn.Module): # Module: Pytorch Base class for all neural net
         x = self.batchNorm4(x)
         return x
 
-  
+
+#FC layers used by RN and CNN_MLP
 class FCOutputModel(nn.Module):
     def __init__(self):
         super(FCOutputModel, self).__init__()
@@ -51,6 +86,8 @@ class FCOutputModel(nn.Module):
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
+
+#Base class with handles for training, testing, and saving model
 class BasicModel(nn.Module):
     def __init__(self, args, name):
         super(BasicModel, self).__init__()
@@ -87,12 +124,8 @@ class RN(BasicModel):
         
         self.relation_type = args.relation_type
         
-        if self.relation_type == 'ternary':
-            ##(number of filters per object+coordinate of object)*3+question vector
-            self.g_fc1 = nn.Linear((24+2)*3+18, 256)
-        else:
-            ##(number of filters per object+coordinate of object)*2+question vector
-            self.g_fc1 = nn.Linear((24+2)*2+18, 256)
+        #(number of filters per object(conv output)+coordinate of object)*2+question vector
+        self.g_fc1 = nn.Linear((24+2)*2+11, 256)
 
         self.g_fc2 = nn.Linear(256, 256)
         self.g_fc3 = nn.Linear(256, 256)
@@ -134,57 +167,30 @@ class RN(BasicModel):
         mb = x.size()[0]
         n_channels = x.size()[1]
         d = x.size()[2]
-        # x_flat = (64 x 25 x 24)
-        x_flat = x.view(mb,n_channels,d*d).permute(0,2,1)
+        
+        x_flat = x.view(mb,n_channels,d*d).permute(0,2,1) # (64 x 25 x 24)
         
         # add coordinates
-        x_flat = torch.cat([x_flat, self.coord_tensor],2)
+        x_flat = torch.cat([x_flat, self.coord_tensor],2) #(64 x 25 x (24+2))
         
 
-        if self.relation_type == 'ternary':
-            # add question everywhere
-            qst = torch.unsqueeze(qst, 1) # (64x1x18)
-            qst = qst.repeat(1, 25, 1) # (64x25x18)
-            qst = torch.unsqueeze(qst, 1)  # (64x1x25x18)
-            qst = torch.unsqueeze(qst, 1)  # (64x1x1x25x18)
+        # add question everywhere
+        qst = torch.unsqueeze(qst, 1) # 64 x 1 x 11
+        qst = qst.repeat(1, 25, 1) # 64 x 25 x 11
+        qst = torch.unsqueeze(qst, 2) # 64 x 25 x 1 x 11
 
-            # cast all triples against each other
-            x_i = torch.unsqueeze(x_flat, 1)  # (64x1x25x26)
-            x_i = torch.unsqueeze(x_i, 3)  # (64x1x25x1x26)
-            x_i = x_i.repeat(1, 25, 1, 25, 1)  # (64x25x25x25x26)
-            
-            x_j = torch.unsqueeze(x_flat, 2)  # (64x25x1x26)
-            x_j = torch.unsqueeze(x_j, 2)  # (64x25x1x1x26)
-            x_j = x_j.repeat(1, 1, 25, 25, 1)  # (64x25x25x25x26)
-
-            x_k = torch.unsqueeze(x_flat, 1)  # (64x1x25x26)
-            x_k = torch.unsqueeze(x_k, 1)  # (64x1x1x25x26)
-            x_k = torch.cat([x_k, qst], 4)  # (64x1x1x25x26+18)
-            x_k = x_k.repeat(1, 25, 25, 1, 1)  # (64x25x25x25x26+18)
-
-            # concatenate all together
-            x_full = torch.cat([x_i, x_j, x_k], 4)  # (64x25x25x25x3*26+18)
-
-            # reshape for passing through network
-            x_ = x_full.view(mb * (d * d) * (d * d) * (d * d), 96)  # (64*25*25*25x3*26+18) = (1.000.000, 96)
-        else:
-            # add question everywhere
-            qst = torch.unsqueeze(qst, 1)
-            qst = qst.repeat(1, 25, 1)
-            qst = torch.unsqueeze(qst, 2)
-
-            # cast all pairs against each other
-            x_i = torch.unsqueeze(x_flat, 1)  # (64x1x25x26+18)
-            x_i = x_i.repeat(1, 25, 1, 1)  # (64x25x25x26+18)
-            x_j = torch.unsqueeze(x_flat, 2)  # (64x25x1x26+18)
-            x_j = torch.cat([x_j, qst], 3)
-            x_j = x_j.repeat(1, 1, 25, 1)  # (64x25x25x26+18)
-            
-            # concatenate all together
-            x_full = torch.cat([x_i,x_j],3) # (64x25x25x2*26+18)
+        # cast all pairs against each other
+        x_i = torch.unsqueeze(x_flat, 1)  # (64 x 1 x 25 x (24+2))
+        x_i = x_i.repeat(1, 25, 1, 1)  # (64 x 25 x 25 x (24+2))
+        x_j = torch.unsqueeze(x_flat, 2)  # (64 x 25 x 1 x (24+2))
+        x_j = torch.cat([x_j, qst], 3) # (64 x 25 x 1 x (24+2 + 11))
+        x_j = x_j.repeat(1, 1, 25, 1)  # (64 x 25 x 25 x (24+2 + 11))
         
-            # reshape for passing through network
-            x_ = x_full.view(mb * (d * d) * (d * d), 70)  # (64*25*25x2*26*18) = (40.000, 70)
+        # concatenate all together
+        x_full = torch.cat([x_i,x_j],3) # (64 x 25 x 25 x ((24+2)*2 + 11))
+    
+        # reshape for passing through network
+        x_ = x_full.view(mb * (d * d) * (d * d), 63)  # (64*25*25x(2*26+11)) = (40.000, 63)
             
         x_ = self.g_fc1(x_)
         x_ = F.relu(x_)
@@ -196,10 +202,7 @@ class RN(BasicModel):
         x_ = F.relu(x_)
         
         # reshape again and sum
-        if self.relation_type == 'ternary':
-            x_g = x_.view(mb, (d * d) * (d * d) * (d * d), 256)
-        else:
-            x_g = x_.view(mb, (d * d) * (d * d), 256)
+        x_g = x_.view(mb, (d * d) * (d * d), 256)
 
         x_g = x_g.sum(1).squeeze()
         
@@ -210,26 +213,132 @@ class RN(BasicModel):
         return self.fcout(x_f)
 
 
+class BiggerRN(BasicModel):
+    def __init__(self, args):
+        super(BiggerRN, self).__init__(args, 'BiggerRN')
+        
+        self.conv = BiggerConvInputModel()
+        
+        self.relation_type = args.relation_type
+        
+        #(number of filters per object+coordinate of object)*2+question vector
+        self.g_fc1 = nn.Linear((256+2)*2+11, 2000)
+
+        self.g_fc2 = nn.Linear(2000, 2000)
+        self.g_fc3 = nn.Linear(2000, 2000)
+        self.g_fc4 = nn.Linear(2000, 2000)
+
+        self.f_fc1 = nn.Linear(2000, 2000)
+        self.f_fc2 = nn.Linear(2000, 1000)
+        self.f_fc3 = nn.Linear(1000, 500)
+        self.f_fc4 = nn.Linear(500, 100)
+        self.f_fc5 = nn.Linear(100, 10) #final output length depends on the answer embedding (10 in this case)
+
+        self.coord_oi = torch.FloatTensor(args.batch_size, 2)
+        self.coord_oj = torch.FloatTensor(args.batch_size, 2)
+        if args.cuda:
+            self.coord_oi = self.coord_oi.cuda()
+            self.coord_oj = self.coord_oj.cuda()
+        self.coord_oi = Variable(self.coord_oi)
+        self.coord_oj = Variable(self.coord_oj)
+
+        # prepare coord tensor
+        def cvt_coord(i):
+            return [(i/5-2)/2., (i%5-2)/2.]
+        
+        self.coord_tensor = torch.FloatTensor(args.batch_size, 25, 2)
+        if args.cuda:
+            self.coord_tensor = self.coord_tensor.cuda()
+        self.coord_tensor = Variable(self.coord_tensor)
+        np_coord_tensor = np.zeros((args.batch_size, 25, 2))
+        for i in range(25):
+            np_coord_tensor[:,i,:] = np.array( cvt_coord(i) )
+        self.coord_tensor.data.copy_(torch.from_numpy(np_coord_tensor))
+
+
+        self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
+
+
+    def forward(self, img, qst):
+        x = self.conv(img) ## x = (64 x 256 x 5 x 5)
+        
+        """g"""
+        mb = x.size()[0]
+        n_channels = x.size()[1]
+        d = x.size()[2]
+        
+        x_flat = x.view(mb,n_channels,d*d).permute(0,2,1) # (64 x 25 x 256)
+        
+        # add coordinates
+        x_flat = torch.cat([x_flat, self.coord_tensor],2) #(64 x 25 x (256 + 2))
+        
+
+        # add question everywhere
+        qst = torch.unsqueeze(qst, 1) #(64,11) -> (64,1,11)
+        qst = qst.repeat(1, 25, 1) # 64 x 25 x 11
+        qst = torch.unsqueeze(qst, 2) #64 x 25 x 1 x 11
+
+        # cast all pairs against each other
+        x_i = torch.unsqueeze(x_flat, 1)  # 64 x 1 x 25 x (256 + 2)
+        x_i = x_i.repeat(1, 25, 1, 1)  # 64 x 25 x 25 x (256 + 2)
+        x_j = torch.unsqueeze(x_flat, 2)  #  64 x 25 x 1 x (256 + 2)
+        x_j = torch.cat([x_j, qst], 3) # 64 x 25 x 1 x (256 + 2 + 11)
+        x_j = x_j.repeat(1, 1, 25, 1)  #  64 x 25 x 25 x (256 + 2 + 11)
+        
+        # concatenate all together
+        x_full = torch.cat([x_i,x_j],3) # 64 x 25 x 25 x ((256 + 2) + (256 + 2 + 11))
+    
+        # reshape for passing through network
+        x_ = x_full.view(mb * (d * d) * (d * d), 2*(256 + 2) + 11)  # (64*25*25 x (2*258+11)) = (40000, 527)
+            
+        x_ = self.g_fc1(x_) # 64*25*25 x 2000
+        x_ = F.relu(x_)
+        x_ = self.g_fc2(x_) # 64*25*25 x 2000
+        x_ = F.relu(x_)
+        x_ = self.g_fc3(x_) # 64*25*25 x 2000
+        x_ = F.relu(x_)
+        x_ = self.g_fc4(x_) # 64*25*25 x 2000
+        x_ = F.relu(x_)
+        
+        # reshape again and sum
+        x_g = x_.view(mb, (d * d) * (d * d), 2000) # 64 x 25*25 x 2000
+
+        x_g = x_g.sum(1).squeeze() # 64 x 2000
+        
+        """f"""
+        x_f = self.f_fc1(x_g) # 64 x 2000
+        x_f = F.relu(x_f)
+        x_f = self.f_fc2(x_f) # 64 x 1000
+        x_f = F.relu(x_f)
+        x_f = self.f_fc3(x_f) # 64 x 500
+        x_f = F.relu(x_f)
+        x_f = self.f_fc4(x_f) # 64 x 100
+        x_f = F.relu(x_f)
+        x_f = self.f_fc5(x_f) # 64 x 10
+        x_f = F.log_softmax(x_f, dim=1) # 64 x 10
+        
+        return x_f
+
+
 class CNN_MLP(BasicModel):
     def __init__(self, args):
         super(CNN_MLP, self).__init__(args, 'CNNMLP')
 
         self.conv  = ConvInputModel()
-        self.fc1   = nn.Linear(5*5*24 + 18, 256)  # question concatenated to all
+        self.fc1   = nn.Linear(5*5*24 + 11, 256)  # question concatenated to all
         self.fcout = FCOutputModel()
 
         self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
-        #print([ a for a in self.parameters() ] )
   
     def forward(self, img, qst):
-        x = self.conv(img) ## x = (64 x 24 x 5 x 5)
+        x = self.conv(img) # x = (64 x 24 x 5 x 5)
 
         """fully connected layers"""
-        x = x.view(x.size(0), -1)
+        x = x.view(x.size(0), -1) # (64 x 24 * 5 * 5)
         
-        x_ = torch.cat((x, qst), 1)  # Concat question
+        x_ = torch.cat((x, qst), 1)  # Concat question (64 x (24 * 5 * 5 + 11))
         
-        x_ = self.fc1(x_)
-        x_ = F.relu(x_)
+        x_ = self.fc1(x_) # (64 x 256)
+        x_ = F.relu(x_) # (64 x 256)
         
-        return self.fcout(x_)
+        return self.fcout(x_) # (64 x 10)
--- sort_of_clevr_generator.py
+++ sort_of_clevr_generator.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import numpy as np
 import random
-#import cPickle as pickle
+
 import pickle
 import warnings
 import argparse
@@ -17,25 +17,19 @@ args = parser.parse_args()
 random.seed(args.seed)
 np.random.seed(args.seed)
 
-# <Change the number of training/test dataset to match the paper>
 train_size = 9800
 test_size = 200
-# train_size = 9500
-# test_size = 500
-
-# ? how do we know these numbers?
 img_size = 75
-size = 5 # Is this question type and subtype?
-question_size = 18  ##? 2 x (6 for one-hot vector of color), 3 for question type, 3 for question subtype
-q_type_idx = 12
-sub_q_type_idx = 15
-
+size = 5
+question_size = 11  ## 1 x (6 for one-hot vector of color), 2 for question type, 3 for question subtype
+q_type_idx = 6
+sub_q_type_idx = 8
 """Answer : [yes, no, rectangle, circle, r, g, b, o, k, y]"""
 
 nb_questions = 10
 dirs = './data'
 
-colors = [ # 6 colors
+colors = [
     (0,0,255),##r
     (0,255,0),##g
     (255,0,0),##b
@@ -50,11 +44,12 @@ try:
 except:
     print('directory {} already exists'.format(dirs))
 
+#pick random centers until new object does not overlap with existing objects
 def center_generate(objects):
     while True:
         pas = True
         center = np.random.randint(0+size, img_size - size, 2)        
-        if len(objects) > 0: # if there is an img
+        if len(objects) > 0:
             for name,c,shape in objects:
                 if ((center - c) ** 2).sum() < ((size * 2) ** 2):
                     pas = False
@@ -62,36 +57,30 @@ def center_generate(objects):
             return center
 
 
+
 def build_dataset():
     objects = []
     img = np.ones((img_size,img_size,3)) * 255
+    
+    #generate 1 object of each color, random shape (Square/Circle), random center, size = 5 
     for color_id,color in enumerate(colors):  
         center = center_generate(objects)
-        if random.random()<0.5: # rectangle
+        if random.random()<0.5:
             start = (center[0]-size, center[1]-size)
             end = (center[0]+size, center[1]+size)
-            cv2.rectangle(img, start, end, color, -1) # Thickness of -1 px will fill the rectangle shape by the specified color.
+            cv2.rectangle(img, start, end, color, -1)
             objects.append((color_id,center,'r'))
-        else: # circle
+        else:
             center_ = (center[0], center[1])
             cv2.circle(img, center_, size, color, -1)
             objects.append((color_id,center,'c'))
 
 
-    ternary_questions = [] # no need
-    binary_questions = [] # encoded questions
-    norel_questions = [] # nonrelational questions
-    ternary_answers = [] # no need
-    binary_answers = [] # encoded answers
-    norel_answers = [] # non relational answers
-    
-    """@@@ Non-relational questions @@@"""
-    '''
-    - <3 subtype>: Non-relational questions
-            - 0: query shape (square or circle)
-            - 1: query horizontal position (left/right of the image)
-            - 2: query vertical position (top/bottom of the image)
-    '''
+    binary_questions = []
+    norel_questions = []
+    binary_answers = []
+    norel_answers = []
+    """Non-relational questions"""
     for _ in range(nb_questions):
         question = np.zeros((question_size))
         color = random.randint(0,5)
@@ -100,32 +89,30 @@ def build_dataset():
         subtype = random.randint(0,2)
         question[subtype+sub_q_type_idx] = 1
         norel_questions.append(question)
-        """Answer : [yes, no, rectangle, circle, r, g, b, o, k, y]"""
+        """Answer : [yes, no, rectangle, circle, 0, 1, 2, 3, 4, 5]"""
         if subtype == 0:
-            """query SHAPE->rectangle/circle"""
-            if objects[color][2] == 'r':  # ? is it because the order is RGB->BGR?
-                answer = 2 # what are these numbers?
+            """query shape->rectangle/circle"""
+            if objects[color][2] == 'r':
+                answer = 2
             else:
                 answer = 3
 
         elif subtype == 1:
-            """query HORIZONTAL POSITION ->yes/no"""
-            '''Left/Right'''
+            """query horizontal position->yes/no"""
             if objects[color][1][0] < img_size / 2:
                 answer = 0
             else:
                 answer = 1
 
         elif subtype == 2:
-            """query VERTICAL POSITION ->yes/no"""
-            '''Top/Bottom(Vertical)'''
+            """query vertical position->yes/no"""
             if objects[color][1][1] < img_size / 2:
                 answer = 0
             else:
                 answer = 1
         norel_answers.append(answer)
     
-    """@@@ Binary Relational questions @@@"""
+    """Binary Relational questions"""
     for _ in range(nb_questions):
         question = np.zeros((question_size))
         color = random.randint(0,5)
@@ -136,10 +123,10 @@ def build_dataset():
         binary_questions.append(question)
 
         if subtype == 0:
-            """CLOSEST-TO-> rectangle/circle (min)""" 
+            """closest-to->rectangle/circle"""
             my_obj = objects[color][1]
             dist_list = [((my_obj - obj[1]) ** 2).sum() for obj in objects]
-            dist_list[dist_list.index(0)] = 999 # ? what is 999
+            dist_list[dist_list.index(0)] = 999
             closest = dist_list.index(min(dist_list))
             if objects[closest][2] == 'r':
                 answer = 2
@@ -147,7 +134,7 @@ def build_dataset():
                 answer = 3
                 
         elif subtype == 1:
-            """FURTHEST-from->rectangle/circle (max)"""
+            """furthest-from->rectangle/circle"""
             my_obj = objects[color][1]
             dist_list = [((my_obj - obj[1]) ** 2).sum() for obj in objects]
             furthest = dist_list.index(max(dist_list))
@@ -157,126 +144,23 @@ def build_dataset():
                 answer = 3
 
         elif subtype == 2:
-            """COUNT->1~6"""
+            """count->1~6"""
+            #how many objects have the same shape as my-object
             my_obj = objects[color][2]
             count = -1
-            for obj in objects: #? objects = a pic containing 6 objects ?
-                if obj[2] == my_obj: # same ?color or shape as well?
+            for obj in objects:
+                if obj[2] == my_obj:
                     count +=1 
             answer = count+4
 
+        #answer is an int  between 0 and 9, needs to be one-hot encoded
         binary_answers.append(answer)
 
-
-'''
-Ternary: NO NEED FOR FINAL PROJECT
-'''
-    """Ternary Relational questions"""
-    for _ in range(nb_questions):
-        question = np.zeros((question_size))
-        rnd_colors = np.random.permutation(np.arange(5))
-        # 1st object
-        color1 = rnd_colors[0]
-        question[color1] = 1
-        # 2nd object
-        color2 = rnd_colors[1]
-        question[6 + color2] = 1
-
-        question[q_type_idx + 2] = 1
-        
-        if args.t_subtype >= 0 and args.t_subtype < 3:
-            subtype = args.t_subtype
-        else:
-            subtype = random.randint(0, 2)
-
-        question[subtype+sub_q_type_idx] = 1
-        ternary_questions.append(question)
-
-        # get coordiates of object from question
-        A = objects[color1][1]
-        B = objects[color2][1]
-
-        if subtype == 0:
-            """between->1~4"""
-
-            between_count = 0 
-            # check is any objects lies inside the box
-            for other_obj in objects:
-                # skip object A and B
-                if (other_obj[0] == color1) or (other_obj[0] == color2):
-                    continue
-
-                # Get x and y coordinate of third object
-                other_objx = other_obj[1][0]
-                other_objy = other_obj[1][1]
-
-                if (A[0] <= other_objx <= B[0] and A[1] <= other_objy <= B[1]) or \
-                   (A[0] <= other_objx <= B[0] and B[1] <= other_objy <= A[1]) or \
-                   (B[0] <= other_objx <= A[0] and B[1] <= other_objy <= A[1]) or \
-                   (B[0] <= other_objx <= A[0] and A[1] <= other_objy <= B[1]):
-                    between_count += 1
-
-            answer = between_count + 4
-        elif subtype == 1:
-            """is-on-band->yes/no"""
-            
-            grace_threshold = 12  # half of the size of objects
-            epsilon = 1e-10  
-            m = (B[1]-A[1])/((B[0]-A[0]) + epsilon ) # add epsilon to prevent dividing by zero
-            c = A[1] - (m*A[0])
-
-            answer = 1  # default answer is 'no'
-
-            # check if any object lies on/close the line between object A and object B
-            for other_obj in objects:
-                # skip object A and B
-                if (other_obj[0] == color1) or (other_obj[0] == color2):
-                    continue
-
-                other_obj_pos = other_obj[1]
-                
-                # y = mx + c
-                y = (m*other_obj_pos[0]) + c
-                if (y - grace_threshold)  <= other_obj_pos[1] <= (y + grace_threshold):
-                    answer = 0
-        elif subtype == 2:
-            """count-obtuse-triangles->1~6"""
-
-            obtuse_count = 0
-
-            # disable warnings
-            # the angle computation may fail if the points are on a line
-            warnings.filterwarnings("ignore")
-            for other_obj in objects:
-                # skip object A and B
-                if (other_obj[0] == color1) or (other_obj[0] == color2):
-                    continue
-
-                # get position of 3rd object
-                C = other_obj[1]
-                # edge length
-                a = np.linalg.norm(B - C)
-                b = np.linalg.norm(C - A)
-                c = np.linalg.norm(A - B)
-                # angles by law of cosine
-                alpha = np.rad2deg(np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)))
-                beta = np.rad2deg(np.arccos((a ** 2 + c ** 2 - b ** 2) / (2 * a * c)))
-                gamma = np.rad2deg(np.arccos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b)))
-                max_angle = max(alpha, beta, gamma)
-                if max_angle >= 90 and max_angle < 180:
-                    obtuse_count += 1
-
-            warnings.filterwarnings("default")
-            answer = obtuse_count + 4
-
-        ternary_answers.append(answer)
-
-    ternary_relations = (ternary_questions, ternary_answers) # no need 
-    binary_relations = (binary_questions, binary_answers) # Relational - Binary
-    norelations = (norel_questions, norel_answers) # Non-relational = Subtype
+    binary_relations = (binary_questions, binary_answers)
+    norelations = (norel_questions, norel_answers)
     
-    img = img/255. # pixel values range from 0 to 256
-    dataset = (img, ternary_relations, binary_relations, norelations)
+    img = img/255.
+    dataset = (img, binary_relations, norelations)
     return dataset
 
 
@@ -286,12 +170,8 @@ print('building train datasets...')
 train_datasets = [build_dataset() for _ in range(train_size)]
 
 
-#img_count = 0
-#cv2.imwrite(os.path.join(dirs,'{}.png'.format(img_count)), cv2.resize(train_datasets[0][0]*255, (512,512)))
-
-
 print('saving datasets...')
-filename = os.path.join(dirs,'sort-of-clevr.pickle')
+filename = os.path.join(dirs,'sort-of-clevr-original.pickle')
 with  open(filename, 'wb') as f:
     pickle.dump((train_datasets, test_datasets), f)
 print('datasets saved at {}'.format(filename))
